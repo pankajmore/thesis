fe76165a2caf383885b8392f5178426bf99cdd0d
Pankaj More <pankajmore@gmail.com>
2014-01-23 10:02:01 +0530

    HelloWorld example working on 32 bit arch

--- experiments/HelloWorld/Main.hs
+++ experiments/HelloWorld/Main.hs
@@ -7,7 +7,6 @@ main :: IO ()
 main = forever $ do
   putStrLn "Loading"
   make "Plug.hs" []
---  mv <- load "Plug.o" [] ["/usr/lib/ghc-7.6.3/package.conf.d","/home/pankajm/.ghc/x86_64-linux-7.6.3/package.conf.d"] "main'"
   mv <- load "Plug.o" [] [] "main'"
   putStrLn "Loaded"
   case mv of
